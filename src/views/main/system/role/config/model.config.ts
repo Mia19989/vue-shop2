d014f5a3fee5dac036adc30566f62fe96b883776
清水 <2756848729@qq.com>
2022-05-23 09:00:35 +0800

    feat(system): 新建角色

---
+++ src/views/main/system/role/config/model.config.ts
@@ -0,0 +1,22 @@
+import { IFormConfig } from '@/base-ui/form'
+export const modelConfig: IFormConfig = {
+  formItems: [
+    {
+      field: 'name',
+      type: 'input',
+      label: '角色名',
+      placeholder: '请输入角色名'
+    },
+    {
+      field: 'intro',
+      type: 'input',
+      label: '权限介绍',
+      placeholder: '请输入权限介绍'
+    }
+  ],
+  itemStyle: {},
+
+  colLayout: {
+    span: 24
+  }
+}
--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -4,7 +4,15 @@
     <page-content
       :contentTableConfig="contentTableConfig"
       pageName="role"
+      @newBtnClick="handleNewClick"
+      @editBtnClick="handleEditClick"
     ></page-content>
+    <page-model
+      ref="pageModelRef"
+      :modelConfig="modelConfig"
+      pageName="role"
+      :defaultInfo="defaultInfo"
+    ></page-model>
   </div>
 </template>
 
@@ -12,21 +20,33 @@
 import { defineComponent } from 'vue'
 import PageContent from '@/components/page-content'
 import PageSearch from '@/components/page-search'
+import PageModel from '@/components/page-model'
 
 import { contentTableConfig } from './config/content.config'
 import { searchFromConfig } from './config/search.config'
+import { modelConfig } from './config/model.config'
+
+import { usePageModel } from '@/hooks/usePageModel'
 
 export default defineComponent({
   name: 'role',
   components: {
     PageContent,
-    PageSearch
+    PageSearch,
+    PageModel
   },
 
   setup() {
+    const [pageModelRef, defaultInfo, handleNewClick, handleEditClick] =
+      usePageModel()
     return {
       contentTableConfig,
-      searchFromConfig
+      searchFromConfig,
+      modelConfig,
+      pageModelRef,
+      defaultInfo,
+      handleNewClick,
+      handleEditClick
     }
   }
 })
